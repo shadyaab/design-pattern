f8e0cb75bf4efc2238c36cefabfbb341ff4054ae
Shadyaab Akhtar <shadyaab.akhtar@lowes.com>
2018-11-16 18:19:37 +0530

    decorator pattern update

--- designpattern/src/main/java/net/sady/designpattern/structural/decorator/TestDecorator.java
+++ designpattern/src/main/java/net/sady/designpattern/structural/decorator/TestDecorator.java
@@ -45,11 +45,11 @@ class Jalapeno extends ToppingDecorator{
 	}
 	
 	public String getDescription() {
-		return null;
+		return pizza.getDescription() + ", Jalapeno";
 	}
 
 	int getPrice() {
-		return 0;
+		return pizza.getPrice() + 10;
 	}
 }
 
@@ -62,23 +62,29 @@ class Corn extends ToppingDecorator{
 	}
 	
 	public String getDescription() {
-		return null;
+		return pizza.getDescription() + ", Corn";
 	}
 
 	int getPrice() {
-		return 0;
+		return pizza.getPrice() + 20;
 	}
-	
 }
 
 
 public class TestDecorator {
 	
 	public static void main(String[] args) {
-		Pizza pizza = new SimplePizza();
+		Pizza pizza = new SimplePizza("Simple Pizza");
 		pizza = new Jalapeno(pizza);
+		System.out.println(pizza.getDescription() + " And Price : "+ pizza.getPrice());
 		
-		Pizza pizza2 = new Corn(new SimplePizza());
+		//Adding sinle topping
+		Pizza pizza2 = new Corn(new SimplePizza("Simple Pizza"));
+		System.out.println(pizza2.getDescription() + " And Price : "+ pizza2.getPrice());
+		
+		//Adding multiple toppings
+		Pizza pizza3 = new Corn(new Jalapeno(new SimplePizza("Simple Pizza")));
+		System.out.println(pizza3.getDescription() + " And Price : "+ pizza3.getPrice());
 		
 	}
 	
