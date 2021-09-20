8d92c1996e1e0b5841b73e88a5a39fa94e9fb780
shadyaab <shadyaab543@gmail.com>
2021-09-20 22:09:14 +0530

    Added Command DP

---
+++ designpattern/src/main/java/net/sady/designpattern/behavior/command/CommandDemo.java
@@ -0,0 +1,98 @@
+package net.sady.designpattern.behavior.command;
+
+import java.io.File;
+
+interface FileExecutor {
+    void openFile();
+    void writeFile();
+    void closeFile();
+}
+
+class FileExecutorImpl implements FileExecutor{
+
+    @Override
+    public void openFile() {
+        System.out.println("Opened File");
+    }
+
+    @Override
+    public void writeFile() {
+        System.out.println("Wrote File");
+    }
+
+    @Override
+    public void closeFile() {
+        System.out.println("Closed File");
+    }
+}
+
+interface Command {
+    void execute();
+}
+
+class OpenFileCommand implements Command {
+
+    FileExecutor fileExecutor = null;
+
+    OpenFileCommand(FileExecutor fileExecutor) {
+        this.fileExecutor = fileExecutor;
+    }
+
+    @Override
+    public void execute() {
+        this.fileExecutor.openFile();
+    }
+}
+
+class WriteFileCommand implements Command {
+
+    FileExecutor fileExecutor = null;
+
+    WriteFileCommand(FileExecutor fileExecutor){
+        this.fileExecutor = fileExecutor;
+    }
+
+    @Override
+    public void execute() {
+        this.fileExecutor.writeFile();
+    }
+}
+
+class Invoker {
+    Command command = null;
+    public Invoker (Command command){
+        this.command = command;
+    }
+    public void execute() {
+        this.command.execute();
+    }
+}
+
+public class CommandDemo {
+    public static void main(String[] args) {
+        OpenFileCommand openFileCommand = new OpenFileCommand(new FileExecutorImpl());
+        Invoker commandInvoker = new Invoker(openFileCommand);
+        commandInvoker.execute();
+
+        new Invoker(new WriteFileCommand(new FileExecutorImpl())).execute();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
